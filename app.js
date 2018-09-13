bf3d112ddce9bd53f179b77a165420b5fca98fa1
Zhang Gavin <gavin.zhang@nokia-sbell.com>
2018-09-13 17:02:18 +0800

    little changes

--- app.js
+++ app.js
@@ -7,3 +7,19 @@ const roads = [
   'Marketplace-Post Office', 'Marketplace-Shop',
   'Marketplace-Town Hall', 'Shop-Town Hall'
 ];
+
+function buildGraph (edges) {
+  let graph = Object.create(null)
+  function addEdge (from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to]
+    } else {
+      graph[from].push(to)
+    }
+  }
+  for (let [from, to] of edges.map(r => r.split('-'))) {
+    addEdge(from, to)
+    addEdge(to, from)
+  }
+  return graph
+}
